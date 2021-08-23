63f8f988bd8aee3385c785eb9270839dc40587e1
Sky10line <rodrigo_ryo@hotmail.com>
2021-08-23 19:17:23 -0300

    Creating project

---
+++ The_Experience/The_Experience/The_ExperienceApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_ExperienceApp.swift
+//  The_Experience
+//
+//  Created by Rodrigo Ryo Aoki on 23/08/21.
+//
+
+import SwiftUI
+
+@main
+struct The_ExperienceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
